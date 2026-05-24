417f3c9d4ea84cc481110b5ba20f01d497835a38
onkar <surwaseonkar12345@gmail.com>
2026-05-24 23:06:43 +0530

    w

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://gabha-studio-04-05-2026.onrender.com/api',
 });
 
 // Add a request interceptor to inject the token
