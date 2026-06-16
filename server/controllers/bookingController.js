a2b4b8c362c7f737d51b16dc0778557d7b5c1ac9
onkar <surwaseonkar12345@gmail.com>
2026-06-16 22:27:27 +0530

    test

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -221,7 +221,7 @@ const generateInvoicePDF = async (req, res) => {
 
     const browser = await puppeteer.launch({
       headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu']
     });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
--- server/controllers/quotationMasterController.js
+++ server/controllers/quotationMasterController.js
@@ -147,7 +147,7 @@ const generateMasterPDF = async (req, res) => {
 
     const browser = await puppeteer.launch({
       headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu']
     });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
